eadc544613f5fb3c5213c498f7e72882fe5fda47
GnomeTV <melnikov.ivan2810@gmail.com>
2020-10-25 17:36:02 +0300

    changed constraint on AuthScreen

--- CAFS/CAFS/PresentationLayer/AuthorizationViewController.swift
+++ CAFS/CAFS/PresentationLayer/AuthorizationViewController.swift
@@ -12,6 +12,7 @@ class AuthorizationViewController: UIViewController {
         createTitleLabel()
         createAndPositionTextField()
         createAndPositionTextFieldIndicator()
+        createAndPositionTitleLabelIndicator()
     }
     
     private func createAndPositionTextField() {
@@ -21,9 +22,9 @@ class AuthorizationViewController: UIViewController {
         emailTextField.heightAnchor.constraint(equalToConstant: 24.0).isActive = true
         emailTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24.0).isActive = true
         emailTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24.0).isActive = true
-        emailTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        emailTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -128.0).isActive = true
         
-        emailTextField.placeholder = "Сюда надо вводить мыло"
+        emailTextField.placeholder = "Введите свой email"
         emailTextField.font = UIFont.systemFont(ofSize: 20.0, weight: .regular)
         emailTextField.textColor = .black
         emailTextField.borderStyle = .none
@@ -36,7 +37,7 @@ class AuthorizationViewController: UIViewController {
         view.addSubview(indicatorView)
         indicatorView.translatesAutoresizingMaskIntoConstraints = false
         
-        indicatorView.heightAnchor.constraint(equalToConstant: 1.0).isActive = true
+        indicatorView.heightAnchor.constraint(equalToConstant: 2.0).isActive = true
         indicatorView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24.0).isActive = true
         indicatorView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24.0).isActive = true
         indicatorView.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 2.0).isActive = true
@@ -55,5 +56,17 @@ class AuthorizationViewController: UIViewController {
         titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24.0).isActive = true
         titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24.0).isActive = true
         titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 48.0).isActive = true
+    }
+    private func createAndPositionTitleLabelIndicator() {
+        let indicatorView = UIView()
+        indicatorView.backgroundColor = UIColor.hseBlue
+        
+        view.addSubview(indicatorView)
+        indicatorView.translatesAutoresizingMaskIntoConstraints = false
+        
+        indicatorView.heightAnchor.constraint(equalToConstant: 2.5).isActive = true
+        indicatorView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24.0).isActive = true
+        indicatorView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -292.0).isActive = true
+        indicatorView.topAnchor.constraint(equalTo: view.topAnchor, constant: 84.0).isActive = true
     }
 }
