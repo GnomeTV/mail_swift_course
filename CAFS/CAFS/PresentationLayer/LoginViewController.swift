e4bf2b09d7de393db82149769835e221ec999901
GnomeTV <melnikov.ivan2810@gmail.com>
2020-10-27 16:30:02 +0300

    change bottonHeight value and loginButton bottom constraint; add login label, email and password text fields

--- CAFS/CAFS/PresentationLayer/LoginViewController.swift
+++ CAFS/CAFS/PresentationLayer/LoginViewController.swift
@@ -14,7 +14,7 @@ class LoginViewController: UIViewController {
     // MARK: - Insets
     private let leftInset: CGFloat = 24.0
     private let rightInset: CGFloat = 24.0
-    private let buttonHeight: CGFloat = 40.0
+    private let buttonHeight: CGFloat = 48.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +27,70 @@ class LoginViewController: UIViewController {
     private func setupViews() {
         setupRegisterButton()
         setupLoginButton()
+        setupLoginLabel()
+        setupLoginPasswordTextField()
+        positionTextFieldIndicators()
+        
+    }
+    private func setupLoginPasswordTextField() {
+            view.addSubview(emailTextField)
+            view.addSubview(passwordTextField)
+            
+            emailTextField.translatesAutoresizingMaskIntoConstraints = false
+            passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+            
+            emailTextField.heightAnchor.constraint(equalToConstant: 24.0).isActive = true
+            emailTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24.0).isActive = true
+            emailTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24.0).isActive = true
+            emailTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -128.0).isActive = true
+            
+            emailTextField.placeholder = "Введите e-mail"
+            emailTextField.font = UIFont.systemFont(ofSize: 20.0, weight: .regular)
+            emailTextField.textColor = .black
+            emailTextField.borderStyle = .none
+            
+            passwordTextField.heightAnchor.constraint(equalToConstant: 24.0).isActive = true
+            passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24.0).isActive = true
+            passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24.0).isActive = true
+            passwordTextField.centerYAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 64.0).isActive = true
+            
+            passwordTextField.placeholder = "Введите пароль"
+            passwordTextField.font = UIFont.systemFont(ofSize: 20.0, weight: .regular)
+            passwordTextField.textColor = .black
+            passwordTextField.borderStyle = .none
+    }
+    
+    private func positionTextFieldIndicators() {
+        emailIndicatorView.backgroundColor = UIColor.hseBlue
+        passwordIndicatorView.backgroundColor = UIColor.hseBlue
+        
+        view.addSubview(passwordIndicatorView)
+        view.addSubview(emailIndicatorView)
+        emailIndicatorView.translatesAutoresizingMaskIntoConstraints = false
+        passwordIndicatorView.translatesAutoresizingMaskIntoConstraints = false
+        
+        emailIndicatorView.heightAnchor.constraint(equalToConstant: 2.0).isActive = true
+        emailIndicatorView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24.0).isActive = true
+        emailIndicatorView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24.0).isActive = true
+        emailIndicatorView.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 3.0).isActive = true
+        
+        passwordIndicatorView.heightAnchor.constraint(equalToConstant: 2.0).isActive = true
+        passwordIndicatorView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24.0).isActive = true
+        passwordIndicatorView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24.0).isActive = true
+        passwordIndicatorView.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 3.0).isActive = true
+    }
+    private func setupLoginLabel() {
+            view.addSubview(titleLabel)
+            titleLabel.translatesAutoresizingMaskIntoConstraints = false
+            
+            titleLabel.text = "Вход"
+            titleLabel.textColor = UIColor.hseBlue
+            titleLabel.font = UIFont.systemFont(ofSize: 32.0, weight: .bold)
+            
+            titleLabel.heightAnchor.constraint(equalToConstant: 36.0).isActive = true
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: rightInset).isActive = true
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -leftInset).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 60.0).isActive = true
     }
     
     private func setupLoginButton() {
@@ -38,7 +102,7 @@ class LoginViewController: UIViewController {
         loginButton.heightAnchor.constraint(equalToConstant: buttonHeight).isActive = true
         loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: leftInset).isActive = true
         loginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -rightInset).isActive = true
-        loginButton.bottomAnchor.constraint(equalTo: registerButton.topAnchor, constant: -16.0).isActive = true
+        loginButton.bottomAnchor.constraint(equalTo: registerButton.topAnchor, constant: -8.0).isActive = true
     }
     
     private func setupRegisterButton() {
@@ -50,7 +114,7 @@ class LoginViewController: UIViewController {
         registerButton.heightAnchor.constraint(equalToConstant: buttonHeight).isActive = true
         registerButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: leftInset).isActive = true
         registerButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -rightInset).isActive = true
-        registerButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -32.0).isActive = true
+        registerButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -42.0).isActive = true
     }
     
     @objc private func loginButtonTapped() {
@@ -60,4 +124,5 @@ class LoginViewController: UIViewController {
     @objc private func registerButtonTapped() {
         print("Register")
     }
+    
 }
