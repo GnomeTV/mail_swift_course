8afed9ae3ca3af83f85a82649711ebf427bc3d12
Pavel <travkinpa@mail.ru>
2020-10-31 22:10:02 +0300

    fix bags

--- CAFS/CAFS/PresentationLayer/ProfileViewController.swift
+++ CAFS/CAFS/PresentationLayer/ProfileViewController.swift
@@ -1,57 +1,29 @@
+//
+//  ProfileViewController.swift
+//  CAFS
+//
+//  Created by Павел Травкин on 31.10.2020.
+//
+
 import UIKit
 
 class ProfileViewController: UIViewController {
-    
-    
-    // MARK: - Views
-    private let firstnameTextField = UITextField()
-    private let firstnameIndicatorView = UIView()
-    private let lastnameTextField = UITextField()
-    private let lastnameIndicatorView = UIView()
-    private let bornTextField = UITextField()
-    private let bornIndicatorView = UIView()
-    private let universityTextField = UITextField()
-    private let universityIndicatorView = UIView()
-    private let profilePhoto = UIImage()
-    private let profilePhotoView = UIImageView()
-    private let preferencesButton = HseStyleButton()
-       
-       // MARK: - Insets
-       
-    private let leftInset: CGFloat = 24.0
-    private let rightInset: CGFloat = 24.0
-    private let buttonHeight: CGFloat = 48.0
-    private let textFieldHeight: CGFloat = 24.0
-    private let indicatorHeight: CGFloat = 2.0
-    private let topInsetTextFieldIndicator: CGFloat = 3.0
-    private let fontSizeTextField: CGFloat = 20.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.title = "Профиль"
-        
-        view.backgroundColor = .white
-        setupViews()
+
+        // Do any additional setup after loading the view.
     }
-       
-       
-       // MARK: - Private methods
-    private func setupViews() {
-        setupSelfinfoLabel()
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
-       
-    private func setupSelfinfoLabel() {
-        view.addSubview(firstnameTextField)
-        view.addSubview(lastnameTextField)
-        view.addSubview(bornTextField)
-        view.addSubview(universityTextField)
-        
-        firstnameTextField.translatesAutoresizingMaskIntoConstraints = false
-        lastnameTextField.translatesAutoresizingMaskIntoConstraints = false
-        bornTextField.translatesAutoresizingMaskIntoConstraints = false
-        universityTextField.translatesAutoresizingMaskIntoConstraints = false
-        
-    }
-       
+    */
+
 }
