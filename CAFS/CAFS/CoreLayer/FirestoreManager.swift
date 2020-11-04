a76f028fcbd053e82270c904463b2a50a99298d6
Pavel <travkinpa@mail.ru>
2020-11-04 22:07:18 +0400

    Fix bags

--- CAFS/CAFS/CoreLayer/FirestoreManager.swift
+++ CAFS/CAFS/CoreLayer/FirestoreManager.swift
@@ -6,57 +6,40 @@
 //
 import UIKit
 import Firebase
-import FirebaseFirestoreSwift
 
 protocol IFirestoreManager{
-    func addDocument(collection : String, params : [String : Any], _ onErrorReceived: @escaping (_ error: Error) -> ())
+    func addDocument(collection : String, params : [String : Any], _ completion: @escaping (_ error: Error?) -> ())
     
-    func deleteDocument(collection : String, id : String, _ onErrorReceived: @escaping (_ error: Error) -> ())
+    func deleteDocument(collection : String, id : String, _ completion: @escaping (_ error: Error?) -> ())
     
-    func editDocument(collection : String, id : String, params : [String : Any], _ onErrorReceived: @escaping (_ error: Error) -> ())
-    
+    func editDocument(collection : String, id : String, params : [String : Any], _ completion: @escaping (_ error: Error?) -> ())
 }
-
 
 class FirestoreManager: IFirestoreManager {
 
     private let db = Firestore.firestore()
     
-    func addDocument(collection : String, params : [String : Any], _ onErrorReceived: @escaping (_ error: Error) -> ()){
+    func addDocument(collection : String, params : [String : Any], _ completion: @escaping (_ error: Error?) -> ()){
         
         let newDocument = db.collection(collection).document()
         
-        newDocument.setData(params) { error
-            in
-                guard let error = error
-            else { return }
-                onErrorReceived(error)
-        }
+        newDocument.setData(params, completion: completion)
         
     }
     
-    func deleteDocument(collection : String, id : String, _ onErrorReceived: @escaping (_ error: Error) -> ()){
+    func deleteDocument(collection : String, id : String, _ completion: @escaping (_ error: Error?) -> ()){
         
-    db.collection(collection).document(id).delete() { error
-        in
-            guard let error = error
-        else { return }
-            onErrorReceived(error)
-    }
+    db.collection(collection).document(id).delete(completion: completion)
+        
     }
     
-    func editDocument(collection : String, id : String, params : [String : Any], _ onErrorReceived: @escaping (_ error: Error) -> ()) {
+    func editDocument(collection : String, id : String, params : [String : Any], _ completion: @escaping (_ error: Error?
+    ) -> ()) {
         
         var newParams = params
         newParams["id"] = id
         
-        db.collection(collection).document(id).setData(newParams) { error
-            in
-                guard let error = error
-            else { return }
-                onErrorReceived(error)
-        }
-        
+        db.collection(collection).document(id).setData(newParams, completion: completion)
         
     }
 }
