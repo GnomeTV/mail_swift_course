e8fca1bb47ae650b5fbf214f02ba8731e4cf4200
Алёна Цеплина <ae_tseplina@mail.ru>
2020-11-07 21:15:14 +0300

    changed FirestoreManager

--- CAFS/CAFS/CoreLayer/FirestoreManager.swift
+++ CAFS/CAFS/CoreLayer/FirestoreManager.swift
@@ -1,30 +1,39 @@
 import UIKit
 import Firebase
+import FirebaseFirestoreSwift
 
 protocol IFirestoreManager{
-    func addDocument(collection: String, params: [String : Any], _ completion: @escaping (_ error: Error?) -> Void)
+    func saveObject<Object: Encodable>(_ objectToSave: Object, toCollection collection: String, _ completion: @escaping (_ error: Error?) -> Void)
     
     func deleteDocument(collection: String, id: String, _ completion: @escaping (_ error: Error?) -> Void)
     
-    func editDocument(collection: String, id: String, params: [String : Any], _ completion: @escaping (_ error: Error?) -> Void)
+    func editObject<Object: Encodable>(_ objectToEdit: Object, inCollection collection: String, withId id: String, _ completion: @escaping (_ error: Error?) -> Void)
 }
 
 class FirestoreManager: IFirestoreManager {
 
     private let db = Firestore.firestore()
     
-    func addDocument(collection: String, params: [String : Any], _ completion: @escaping (_ error: Error?) -> Void){
-        let newDocument = db.collection(collection).document()
-        newDocument.setData(params, completion: completion)
+    func saveObject<Object: Encodable>(_ objectToSave: Object, toCollection collection: String, _ completion: @escaping (_ error: Error?) -> Void) {
+        let document = db.collection(collection).document()
+        do {
+            try document.setData(from: objectToSave)
+            completion(nil)
+        } catch {
+            completion(error)
+        }
     }
     
     func deleteDocument(collection: String, id: String, _ completion: @escaping (_ error: Error?) -> Void) {
         db.collection(collection).document(id).delete(completion: completion)
     }
     
-    func editDocument(collection: String, id: String, params: [String : Any], _ completion: @escaping (_ error: Error?) -> Void) {
-        var newParams = params
-        newParams["id"] = id
-        db.collection(collection).document(id).setData(newParams, completion: completion)
+    func editObject<Object: Encodable>(_ objectToEdit: Object, inCollection collection: String, withId id: String, _ completion: @escaping (_ error: Error?) -> Void) {
+        do {
+            try db.collection(collection).document(id).setData(from: objectToEdit)
+            completion(nil)
+        } catch {
+            completion(error)
+        }
     }
 }
