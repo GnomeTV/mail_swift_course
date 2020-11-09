69121f5431add8202ef04f1675f45d100b4f8b68
Kuzmin Dmitry <33459102+Arhangel69@users.noreply.github.com>
2020-11-09 20:02:38 +0300

    Resolve merge conflicts

--- CAFS/CAFS/PresentationLayer/LoginScreen/LoginViewController.swift
+++ CAFS/CAFS/PresentationLayer/LoginScreen/LoginViewController.swift
@@ -3,18 +3,20 @@ import UIKit
 class LoginViewController: UIViewController {
     
     // MARK: - Views
+    
+    private let registrationViewController = RegistrationViewController()
+    private let loginStackiew = UIStackView()
     private let titleLabel = UILabel()
-    private let emailTextField = UITextField()
-    private let emailIndicatorView = UIView()
-    private let passwordTextField = UITextField()
-    private let passwordIndicatorView = UIView()
+    private let emailTextField = UnderlineTextField()
+    private let passwordTextField = UnderlineTextField()
     private let loginButton = HseStyleButton()
     private let registerButton = HseStyleButton()
     
     // MARK: - Insets
+    
     private let leftInset: CGFloat = 24.0
     private let rightInset: CGFloat = 24.0
-    private let buttonHeight: CGFloat = 40.0
+    private let buttonHeight: CGFloat = 48.0
     
     private let model = viewModels.loginViewModel
     
@@ -39,6 +41,40 @@ class LoginViewController: UIViewController {
     private func setupViews() {
         setupRegisterButton()
         setupLoginButton()
+        setupLoginLabel()
+        setupStackView()
+        
+    }
+
+    private func setupLoginLabel() {
+            view.addSubview(titleLabel)
+            titleLabel.translatesAutoresizingMaskIntoConstraints = false
+            
+            titleLabel.text = "Вход"
+            titleLabel.textColor = UIColor.hseBlue
+            titleLabel.font = UIFont.systemFont(ofSize: 32.0, weight: .bold)
+            
+            titleLabel.heightAnchor.constraint(equalToConstant: 36.0).isActive = true
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: rightInset).isActive = true
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -leftInset).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 60.0).isActive = true
+    }
+    
+    private func setupStackView() {
+        view.addSubview(loginStackiew)
+        loginStackiew.translatesAutoresizingMaskIntoConstraints = false
+        
+        loginStackiew.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: rightInset).isActive = true
+        loginStackiew.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -leftInset).isActive = true
+        loginStackiew.topAnchor.constraint(equalTo: view.topAnchor, constant: 326.0).isActive = true
+        
+        emailTextField.placeholder = "Введите e-mail"
+        passwordTextField.placeholder = "Введите пароль"
+        loginStackiew.addArrangedSubview(emailTextField)
+        loginStackiew.addArrangedSubview(passwordTextField)
+        loginStackiew.axis = .vertical
+        loginStackiew.spacing = 50.0
+        
     }
     
     private func setupLoginButton() {
@@ -50,7 +86,7 @@ class LoginViewController: UIViewController {
         loginButton.heightAnchor.constraint(equalToConstant: buttonHeight).isActive = true
         loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: leftInset).isActive = true
         loginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -rightInset).isActive = true
-        loginButton.bottomAnchor.constraint(equalTo: registerButton.topAnchor, constant: -16.0).isActive = true
+        loginButton.bottomAnchor.constraint(equalTo: registerButton.topAnchor, constant: -8.0).isActive = true
     }
     
     private func setupRegisterButton() {
@@ -62,6 +98,16 @@ class LoginViewController: UIViewController {
         registerButton.heightAnchor.constraint(equalToConstant: buttonHeight).isActive = true
         registerButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: leftInset).isActive = true
         registerButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -rightInset).isActive = true
-        registerButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -32.0).isActive = true
+        registerButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -42.0).isActive = true
     }
+    
+    @objc private func loginButtonTapped() {
+        print("Login")
+    }
+    
+    @objc private func registerButtonTapped() {
+        present(registrationViewController, animated: true, completion: nil)
+        print("Register")
+    }
+    
 }
---
+++ CAFS/CAFS/PresentationLayer/RegistrationViewController.swift
@@ -0,0 +1,102 @@
+import UIKit
+
+
+class RegistrationViewController: UIViewController {
+    
+    // MARK: - Views
+    
+    private let registrationStackView = UIStackView()
+    private let titleLabel = UILabel()
+    private let emailTextField = UnderlineTextField()
+    private let passwordTextField = UnderlineTextField()
+    private let repeatPasswordTextField = UnderlineTextField()
+    private let firstNameTextField = UnderlineTextField()
+    private let secondNameTextField = UnderlineTextField()
+    private let universityiTextField = UnderlineTextField()
+    private let loginButton = HseStyleButton()
+    private let registerButton = HseStyleButton()
+    
+    private let checkBoxView = UIView()
+    private let statusStudentButton = CheckBoxButton()
+    private let statusTeacherButton = CheckBoxButton()
+    
+    // MARK: - Insets
+    
+    private let leftInset: CGFloat = 24.0
+    private let rightInset: CGFloat = 24.0
+    private let buttonHeight: CGFloat = 48.0
+    private let topInsetTextFieldIndicator: CGFloat = 3.0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupViews()
+    }
+    
+    // MARK: - Private methods
+    private func setupViews() {
+        setupRegistrationLabel()
+        setupRegisterButton()
+        setupStackView()
+    }
+    
+    private func setupStackView() {
+        view.addSubview(registrationStackView)
+        registrationStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        registrationStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: rightInset).isActive = true
+        registrationStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -leftInset).isActive = true
+        registrationStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 184.0).isActive = true
+        
+        firstNameTextField.placeholder = "Имя"
+        secondNameTextField.placeholder = "Фамилия"
+        universityiTextField.placeholder = "Университет"
+        emailTextField.placeholder = "Введите e-mail"
+        passwordTextField.placeholder = "Введите пароль"
+        repeatPasswordTextField.placeholder = "Повторите пароль"
+        
+        checkBoxView.addSubview(statusTeacherButton)
+        checkBoxView.addSubview(statusStudentButton)
+        
+        registrationStackView.addArrangedSubview(firstNameTextField)
+        registrationStackView.addArrangedSubview(secondNameTextField)
+        registrationStackView.addArrangedSubview(universityiTextField)
+        registrationStackView.addArrangedSubview(checkBoxView)
+        registrationStackView.addArrangedSubview(emailTextField)
+        registrationStackView.addArrangedSubview(passwordTextField)
+        registrationStackView.addArrangedSubview(repeatPasswordTextField)
+        registrationStackView.axis = .vertical
+        registrationStackView.spacing = 50.0
+    }
+    
+    private func setupRegistrationLabel() {
+        view.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        titleLabel.text = "Регистрация"
+        titleLabel.textColor = UIColor.hseBlue
+        titleLabel.font = UIFont.systemFont(ofSize: 32.0, weight: .bold)
+
+        titleLabel.heightAnchor.constraint(equalToConstant: 36.0).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: rightInset).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -leftInset).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 60.0).isActive = true
+    }
+    
+    private func setupRegisterButton() {
+        view.addSubview(registerButton)
+        registerButton.addTarget(self, action: #selector(registerButtonTapped), for: .touchUpInside)
+        registerButton.setTitle("Зарегистрироваться", for: .normal)
+        registerButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        registerButton.heightAnchor.constraint(equalToConstant: buttonHeight).isActive = true
+        registerButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: leftInset).isActive = true
+        registerButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -rightInset).isActive = true
+        registerButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -54.0).isActive = true
+    }
+    
+    @objc private func registerButtonTapped() {
+        dismiss(animated: true, completion: {})
+        print("Register")
+    }
+}
