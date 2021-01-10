f9f5c068ae0fc7cd3620069d936dc846d38a9be7
Kuzmin Dmitry <33459102+Arhangel69@users.noreply.github.com>
2021-01-10 14:12:46 +0300

    Update UserDefaultsManager.swift

--- CAFS/CAFS/CoreLayer/UserDefaultsManager.swift
+++ CAFS/CAFS/CoreLayer/UserDefaultsManager.swift
@@ -10,18 +10,31 @@ protocol IUserDeafaultsManager {
 
 class UserDefaultsManager: IUserDeafaultsManager {
     lazy var userInfo = UserDefaults.standard
-    let key = "userInfo"
+    let keyUserInfo = "userInfo"
+    let keyUserAvatar = "userAvatar"
     
     func updateUserInfo(userData: PersonalData) {
         userInfo.set(true, forKey: "isLogged")
         let storageData = try? JSONEncoder().encode(userData)
-        userInfo.set(storageData, forKey: key)
+        userInfo.set(storageData, forKey: keyUserInfo)
+    }
+    
+    func updateUserAvatar(avatar: Data) {
+        userInfo.set(avatar, forKey: keyUserAvatar)
     }
     
     func getUserInfo() -> PersonalData? {
-        if let rawData = userInfo.data(forKey: key) {
+        if let rawData = userInfo.data(forKey: keyUserInfo) {
             let userData = try? JSONDecoder().decode(PersonalData.self, from: rawData)
             return userData
+        } else {
+            return nil
+        }
+    }
+    
+    func getUserAvatar() -> Data? {
+        if let userAvatar = userInfo.data(forKey: keyUserAvatar) {
+            return userAvatar
         } else {
             return nil
         }
